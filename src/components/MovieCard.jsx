503ac14a2e8c848eaebf6cdfbfa2670e0d9c1de1
uniquesp <sakshisp1342002@gmail.com>
2024-10-24 00:36:14 +0530

    Api fetched

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const MovieCard = () => {
+const MovieCard = ({poster_path,name}) => {
   return (
-    <div className='h-[50vh] w-[200px] bg-center bg-cover rounded-xl hover:scale-110 durarion-300 hover:cursor-pointer' style={{backgroundImage : `url(https://imgs.search.brave.com/GY2jlDj_HgPHvQIniLBoXQzT3UrXS_cx45Rz17YvXtY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/cGhvdG9tdXJhbC5j/b20vbWVkaWEvY2F0/YWxvZy9wcm9kdWN0/L2NhY2hlLzIvaW1h/Z2UvNzAweC9iYmI1/YjBlYzQ5YjQ1OTlh/NjhiZTA0OTZkZjE1/ZWYyYy80Ly0vNC00/MTI0X2F2ZW5nZXJz/X21vdmllX3Bvc3Rl/cl93ZWIuanBn)`}}> 
-      
+    <div className='h-[50vh] w-[200px] bg-center bg-cover rounded-xl hover:scale-110 durarion-300 hover:cursor-pointer' style={{backgroundImage : `url(https://image.tmdb.org/t/p/original/${poster_path})`}}> 
+      <div className='text-white text-xl text-center w-full bg-gray-900/60 p-4'>
+        {name}
+      </div>
     </div>
   )
 }
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,23 +1,37 @@
-import React from 'react'
-import MovieCard from './MovieCard'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import MovieCard from "./MovieCard";
 
 const Movies = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=39fba5af64ba3c9c0ce210bee2b374e5&language=en-US&page=1`
+      )
+      .then(function (res) {
+        console.log(res.data.results);
+        setMovies(res.data.results);
+      });
+  }, []);
+
   return (
-    <div className='p-5'>
-      <div className='text-2xl font-bold m-5 text-center'>
-        Trending Movies
-      </div>
-      <div className='flex flex-row flex-wrap justify-around'>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
+    <div className="p-5">
+      <div className="text-2xl font-bold m-5 text-center">Trending Movies</div>
+      <div className="flex flex-row flex-wrap justify-around gap-8">
+        {movies.map((movieObj) => {
+          return (
+            <MovieCard
+              poster_path={movieObj.poster_path}
+              name={movieObj.original_title}
+            />
+          );
+        })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Movies
+export default Movies;
+//https://api.themoviedb.org/3/movie/popular?api_key=39fba5af64ba3c9c0ce210bee2b374e5&language=en-US&page=1
