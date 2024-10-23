14e456b9e3c6e7a9e272ed9401bac5788b68928a
uniquesp <sakshisp1342002@gmail.com>
2024-10-23 21:19:34 +0530

    MovieCard Added

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import MovieCard from './MovieCard'
 
 const Movies = () => {
   return (
-    <div>
-      <div>
+    <div className='p-5'>
+      <div className='text-2xl font-bold m-5 text-center'>
         Trending Movies
+      </div>
+      <div className='flex flex-row flex-wrap justify-around'>
+        <MovieCard/>
+        <MovieCard/>
+        <MovieCard/>
+        <MovieCard/>
+        <MovieCard/>
+        <MovieCard/>
+        <MovieCard/>
       </div>
     </div>
   )
